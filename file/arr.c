54faea9aebcb6c4a75b40d26be0ae6a2651b6ead
amankumar0199 <amankumar080199@gmail.com>
2018-10-06 02:52:14 +0530

    first commit

---
+++ file/arr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n ,i,sum=0;
+int a[100];
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{sum=sum+a[i];
+}
+printf("%d",sum);
+getch();
+}
